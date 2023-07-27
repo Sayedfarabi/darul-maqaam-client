f3d97b7a3f8fe6a2e3726a692051c7f6769bacf3
Sayedfarabi <sayedfarabi6099@gmail.com>
2023-07-27 23:46:09 +0600

    card update

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BiCartDownload } from 'react-icons/bi';
-import { GiEternalLove } from 'react-icons/gi';
-import { Link } from 'react-router-dom';
+import { GiLoveMystery } from 'react-icons/gi';
+import { BsFillEyeFill } from 'react-icons/bs';
 
-const ProductCard = ({ data, wishHandle, userHaveWishList, userHaveBookings, user, cartHandle, }) => {
+const ProductCard = () => {
+
     return (
-
-
         <div className="group relative item-center justify-center overflow-hidden cursor-default hover:shadow-xl hover:shadow-black/30 trans transition-shadow mx-auto rounded-md">
             <div className="h-96 w-72">
                 <img className='h-full w-full object-cover group-hover:rotate-3 group-hover:scale-125 transition-transform duration-500 rounded-md' src="https://i.pinimg.com/originals/09/83/77/098377222318e9007d396bd78b6d6033.jpg" alt="Product" />
@@ -14,20 +13,17 @@ const ProductCard = ({ data, wishHandle, userHaveWishList, userHaveBookings, use
             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
 
             </div>
-            <div className="absolute inset-0 flex justify-end mt-2 mr-2 ">
-                <div className=''>
-                    <p>
-                        <span className="badge text-red-600 badge-md">{"-40%"}</span>
-                    </p>
+            <div className="absolute inset-0 flex justify-start">
+                <div className='mt-2 ml-2'>
+                    <span className="badge text-red-600 badge-md">{"-40%"}</span>
                 </div>
-
             </div>
 
             <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[70%] md:translate-y-[70%] group-hover:translate-y-0 transition-all">
 
                 <div>
                     <h1 className='text-xl font-semibold text-white mb-2'>
-                        Dr. Kkhandakar Abdur Rahman
+                        Dr. Kkhandakar
                     </h1>
                 </div>
                 <div>
@@ -69,12 +65,14 @@ const ProductCard = ({ data, wishHandle, userHaveWishList, userHaveBookings, use
                     {
                         // user?.uid &&
                         <div className='w-full'>
-                            <button onClick={() => wishHandle(data)}
-                                className=' btn btn-sm bg-yellow-500 hover:bg-red-500 text-2xl capitalize text-black hover:text-white mx-4' disabled={userHaveWishList && true}><GiEternalLove></GiEternalLove></button>
+                            <button
+                                className=' btn btn-sm text-2xl capitalize bg-green-500 hover:bg-green-600 duration-500 text-black hover:text-white mx-4'><BiCartDownload></BiCartDownload>
+                            </button>
 
-                            <button onClick={() => cartHandle(data)}
-                                className=' btn btn-sm bg-lime-400 hover:bg-red-500  text-2xl capitalize text-black hover:text-white  mx-4'
-                                disabled={userHaveBookings && true}><BiCartDownload></BiCartDownload></button>
+                            <button
+                                className=' btn btn-sm text-2xl bg-slate-500 hover:bg-slate-600 capitalize text-black hover:text-white duration-500  mx-4'>
+                                <BsFillEyeFill></BsFillEyeFill>
+                            </button>
                         </div>
                     }
 
